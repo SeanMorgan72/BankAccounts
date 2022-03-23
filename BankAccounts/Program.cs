b04556bee991f3d48fb8047108eab089caa1f9a6
Sean Morgan <scm0323@att.net>
2022-03-23 16:00:29 -0500

    Add project files.

---
+++ BankAccounts/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankAccounts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("Sean Morgan", 1000.00m);
+
+            Console.WriteLine($"Account Owner: {account.Owner}\tAccount# {account.Number}");
+
+            account.MakeDeposit(4530.25m, DateTime.Now, "Paycheck");
+
+            account.MakeWithdrawal(1275.00m, DateTime.Now, "Pay mortgage");
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
